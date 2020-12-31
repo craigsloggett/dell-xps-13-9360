30b81bf059e032e24c4ad7fb3e4ba786b1b9fa7f
Craig Sloggett <craigsloggett@gmail.com>
2020-12-31 14:30:04 -0500

    updates

--- lib/chroot.sh
+++ lib/chroot.sh
@@ -18,23 +18,23 @@ chroot_helper() {
     [ -z "${target:-}" ] && { printf 'Target must be present.\n'; return 1; }
     [ -d "${target:-}" ] || { printf 'Target must be a valid directory.\n'; return 1; }
 
+    # Send a command if present, otherwise use the defaults (interactive).
+    [ -n "${*:-}" ] && set -- -c "$*"
+    set -- /bin/sh -l "$@"
+
+    set -- HOME="${username:+/home}/${username:-root}" "$@"
+    set -- TERM="$TERM" "$@"
+    set -- SHELL=/bin/sh "$@"
+    set -- USER="${username:-root}" "$@"
+    set -- CFLAGS="${CFLAGS:--march=native -mtune=generic -pipe -Os}" "$@"
+    set -- CXXFLAGS="${CXXFLAGS:--march=native -mtune=generic -pipe -Os}" "$@"
+    set -- MAKEFLAGS="${MAKEFLAGS:--j$(nproc 2>/dev/null || printf '1')}" "$@"
+    set -- /usr/bin/env -i "$@"
+
+    set -- "$target" "$@"
+
     # Specify the username if present, otherwise use the defaults (root).
-    [ -n "${username:-}" ] && chroot_args=(--userspec $username)
+    [ -n "${username:-}" ] && set -- --userspec "$username" -- "$@"
 
-    # Set the environment variables.
-    env=( HOME="${username:+/home}/${username:-root}" \
-          TERM="$TERM" \
-          SHELL=/bin/sh \
-          USER="${username:-root}" \
-          CFLAGS="${CFLAGS:--march=native -mtune=generic -pipe -Os}" \
-          CXXFLAGS="${CXXFLAGS:--march=native -mtune=generic -pipe -Os}" \
-          MAKEFLAGS="${MAKEFLAGS:--j$(nproc 2>/dev/null || printf '1')}"
-        )
-
-    # Send a command if present, otherwise login.
-    [ -n "${*:-}" ] && cmd=(-c "$*")
-
-    chroot "${chroot_args[@]}" -- "$target" \
-        /usr/bin/env -i "${env[@]}" \
-        /bin/sh -l "${cmd[@]}"
+    chroot "$@"
 }
