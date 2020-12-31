55faa853fad9e192f762e5f544e19dd6d3801be7
Craig Sloggett <craigsloggett@gmail.com>
2020-12-31 12:59:24 -0500

    updates

--- install.sh
+++ install.sh
@@ -60,6 +60,9 @@ chroot_helper () {
     # Parse the options with `getopts`
     # Pull out optional parameter, username.
     # -> if username is omitted, use default of root.
+    # Do some validation.
+    # Setup our environment variables.
+    # chroooooot!
 
     # getopts /optstring/ /name/ [/arg/...]
     while getopts :t:u: name; do
@@ -79,17 +82,23 @@ chroot_helper () {
     # Specify the username if present, otherwise use the defaults (root).
     [ -n "${username:-}" ] && chroot_args=(--userspec $username)
 
+    # Set the environment variables.
+    env=( HOME="${username:+/home}/${username:-root}" \
+          TERM="$TERM" \
+          SHELL=/bin/sh \
+          USER="${username:-root}" \
+          CFLAGS="${CFLAGS:--march=x86-64 -mtune=generic -pipe -Os}" \
+          CXXFLAGS="${CXXFLAGS:--march=x86-64 -mtune=generic -pipe -Os}" \
+          MAKEFLAGS="${MAKEFLAGS:--j$(nproc 2>/dev/null || printf '1')}"
+        )
+
+    # Send a command based on what was supplied as input parameters.
+    [ -n "${*:-}" ] && cmd=(-c "$*")
+
     # A simple chroot wrapper to execute commands in the new environment.	
     chroot "${chroot_args[@]}" -- "$target" \
-        /usr/bin/env -i \
-            HOME="${username:+/home}/${username:-root}" \
-            TERM="$TERM" \
-            SHELL=/bin/sh \
-            USER="${username:-root}" \
-            CFLAGS="${CFLAGS:--march=x86-64 -mtune=generic -pipe -Os}" \
-            CXXFLAGS="${CXXFLAGS:--march=x86-64 -mtune=generic -pipe -Os}" \
-            MAKEFLAGS="${MAKEFLAGS:--j$(nproc 2>/dev/null || printf '1')}" \
-        /bin/sh -c "$@"
+        /usr/bin/env -i "${env[@]}" \
+        /bin/sh "${cmd[@]}"
 }
 
 setup_repo_directory() {
