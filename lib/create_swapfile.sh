b39f6e29dee2e429da4fa215fdf9d16c8f9fca45
Craig Sloggett <craigsloggett@gmail.com>
2021-01-03 16:17:26 -0500

    updates

--- lib/create_swapfile.sh
+++ lib/create_swapfile.sh
@@ -5,10 +5,12 @@
 create_swapfile() {
     # Check the blocksize of the given root filesystem.
     block_size="$(stat -fc %s $1)"
+    mem_total_kb="$(grep MemTotal /proc/meminfo | awk '{print $2}')"
+    block_count="$(( mem_total_kb * 1024 / block_size ))"
 
     mkdir -p "$1/var"
 
-    dd if=/dev/zero of="$1/var/swapfile" bs=$block_size count=$(( block_size * 1024 ))
+    dd if=/dev/zero of="$1/var/swapfile" bs="$block_size" count="$block_count"
 
     chmod 600 "$1/var/swapfile"
     mkswap "$1/var/swapfile"
